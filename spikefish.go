8af961c9f2e2664029cf3e188db47ecce07ab65c
Bruno Aguirre <yo@brunoaguirre.com>
2011-11-25 07:29:03 -0200

    Read pubkey and passwd

--- spikefish.go
+++ spikefish.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+  "fmt"
   "io/ioutil"
-  . "exp/ssh"
+  "exp/ssh"
 )
 
+func passwordAuth(user, passwd string) bool {
+  fmt.Printf(passwd)
+  return true
+}
+
+func pubKeyAuth(user, algorithm string, pubkey []byte) bool {
+  fmt.Printf("user: %s, alg: %s, pub: %x", user, algorithm, pubkey)
+  return true
+}
+
 func main() {
-  config := new(ServerConfig)
+  config := new(ssh.ServerConfig)
+  config.PasswordCallback = passwordAuth
+  config.PubKeyCallback = pubKeyAuth
+
   pemBytes, err := ioutil.ReadFile("fixtures/id_rsa_test")
+
   if err != nil {
     panic("Failed to load private key" + err.Error())
   }
+
   err = config.SetRSAPrivateKey(pemBytes)
   if err != nil {
     panic("Failed to parse private key" + err.Error())
   }
-  listener, _ := Listen("tcp", "0.0.0.0:2022", config)
+
+  listener, _ := ssh.Listen("tcp", "0.0.0.0:2022", config)
+
+  fmt.Println("SpikeFish")
+  fmt.Println("    listening for incoming connection on :2022")
+
   conn, err := listener.Accept()
+
   if err != nil {
     panic("failed to accept incoming connection")
   }
